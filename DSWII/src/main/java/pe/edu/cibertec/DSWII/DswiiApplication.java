1a9d84e0b6b1597d0315f6d39b5c9bf5bc507527
Enzo008 <enzoaguirre629@gmail.com>
2024-06-17 13:30:01 -0500

    Initial Commit restore

---
+++ DSWII/src/main/java/pe/edu/cibertec/DSWII/DswiiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiApplication.class, args);
+	}
+
+}
---
+++ DSWII/src/test/java/pe/edu/cibertec/DSWII/DswiiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
