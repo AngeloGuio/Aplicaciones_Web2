6be2502658aa71fe72e6dcac229c11b4f0f8d4dd
julioMoscol <juliomoscolgonzales@gmail.com>
2024-07-03 03:59:46 -0500

    inf contacto y nosotros

--- DAWII/src/app/dashboard/contacto/contacto.component.ts
+++ DAWII/src/app/dashboard/contacto/contacto.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
